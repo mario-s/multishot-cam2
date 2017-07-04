5ac0dc12c5f17b7839d3b53487381a3a75e93f06
Mario-S <spindizzy@outlook.com>
2017-07-04 21:05:43 +0200

    new test

--- app/src/test/kotlin/de/mario/camera/CameraFragmentTest.kt
+++ app/src/test/kotlin/de/mario/camera/CameraFragmentTest.kt
@@ -1,6 +1,7 @@
 package de.mario.camera
 
 
+import android.view.View
 import org.hamcrest.CoreMatchers.notNullValue
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -10,6 +11,9 @@ import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 
 import org.junit.Assert.assertThat
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.*
 
 /**
  */
@@ -23,6 +27,18 @@ object CameraFragmentTest : Spek({
         it("should have a factory method to create the fragment") {
             val instance = CameraFragment.newInstance()
             assertThat(instance, notNullValue())
+        }
+
+        it("onViewCreated should request texture view") {
+            val instance = CameraFragment.newInstance()
+            val view = mock(View::class.java)
+            val other = mock(View::class.java)
+            val textureView = mock(AutoFitTextureView::class.java)
+            given(view.findViewById(anyInt())).willReturn(other)
+            given(view.findViewById(R.id.texture)).willReturn(textureView)
+
+            instance.onViewCreated(view, null)
+            verify(view, times(3)).findViewById(anyInt())
         }
 
     }
