d756d13f18f1b2846e5c322a92efa7b0dcecddd4
Mario-S <spindizzy@outlook.com>
2017-07-05 19:56:58 +0200

    initial for settings

---
+++ app/src/main/kotlin/de/mario/camera/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package de.mario.camera
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ *
+ */
+class SettingsActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        fragmentManager.beginTransaction()
+                .replace(android.R.id.content, SettingsFragment())
+                .commit()
+
+    }
+}
---
+++ app/src/main/kotlin/de/mario/camera/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package de.mario.camera
+
+import android.os.Bundle
+import android.preference.PreferenceFragment
+
+/**
+ */
+class SettingsFragment : PreferenceFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
