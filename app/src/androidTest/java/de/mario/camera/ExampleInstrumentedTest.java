22cc8b2f66661a73ffcbb0676e361c42da42c91b
Mario-S <spindizzy@outlook.com>
2017-08-02 22:19:12 +0200

    update test package

--- app/src/androidTest/java/de/mario/camera/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/de/mario/camera/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package photo.mario.de.camera;
+package de.mario.camera;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("photo.mario.de.multishotcamera", appContext.getPackageName());
+        assertEquals("de.mario.camera", appContext.getPackageName());
     }
 }
