809167612294ccf676f8141864fc6fb2e2f58aed
Mario-S <spindizzy@outlook.com>
2018-01-29 18:02:42 +0100

    updated message after processing

--- app/src/main/kotlin/de/mario/camera/message/BroadcastingReceiver.kt
+++ app/src/main/kotlin/de/mario/camera/message/BroadcastingReceiver.kt
@@ -5,13 +5,17 @@ import android.content.Context
 import android.content.Intent
 import de.mario.camera.R
 import de.mario.camera.glue.CameraControlable
+import java.io.File
+import java.lang.String.format
 
 class BroadcastingReceiver(private val control: CameraControlable) : BroadcastReceiver(){
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == string(R.string.EXPOSURE_MERGE)) {
             val result = intent.getStringExtra(string(R.string.MERGED))
-            control.showToast(result)
+            val path = File(result).parent
+            val msg = string(R.string.fusion_saved)
+            control.showToast(format(msg, path))
         }
     }
 
